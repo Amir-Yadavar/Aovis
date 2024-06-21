b8b525565cc702efced07438dd4b2f60ec239d06
amir <amiresya@gmail.com>
2024-06-21 18:13:24 +0330

    add icon menu in size mobile ..

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -3,46 +3,54 @@ import styles from "@/styles/navbar.module.css";
 import { RiMovie2Fill } from "react-icons/ri";
 import { FiUser } from "react-icons/fi";
 import { IoIosSearch } from "react-icons/io";
+import { CgMenuGridO } from "react-icons/cg";
 
 function Navbar() {
   return (
     <header className={styles.header}>
-      {/* navbar in large */}
-      <Link href={'/'} className="d-flex align-items-center">
+      <Link href={"/"} className="d-flex align-items-center">
         <span className={styles.logo}>
           <RiMovie2Fill />
         </span>
         <h2 className={styles.logoTitle}>aovis</h2>
       </Link>
 
-      <nav className={styles.navbar}>
-        <Link href={""} className={styles.navItem}>
-          Home
-        </Link>
-        <Link href={""} className={styles.navItem}>
-          Movies
-        </Link>
-        <Link href={""} className={styles.navItem}>
-          Events
-        </Link>
-        <Link href={""} className={styles.navItem}>
-          Pages
-        </Link>
-        <Link href={""} className={styles.navItem}>
-          News
-        </Link>
-        <Link href={""} className={styles.navItem}>
-          Contact
-        </Link>
-      </nav>
+      {/* navbar in large */}
+      <div className="d-none d-md-block">
+        <nav className={styles.navbar}>
+          <Link href={""} className={styles.navItem}>
+            Home
+          </Link>
+          <Link href={""} className={styles.navItem}>
+            Movies
+          </Link>
+          <Link href={""} className={styles.navItem}>
+            Events
+          </Link>
+          <Link href={""} className={styles.navItem}>
+            Pages
+          </Link>
+          <Link href={""} className={styles.navItem}>
+            News
+          </Link>
+          <Link href={""} className={styles.navItem}>
+            Contact
+          </Link>
+        </nav>
+      </div>
+
+      
 
       <div className="d-flex align-items-center">
         <span className={styles.iconNav}>
-        <IoIosSearch />
+          <IoIosSearch />
         </span>
         <Link href={""} className={styles.iconNav}>
           <FiUser />
         </Link>
+        <div className="d-md-none">
+          <span className={styles.iconNav}><CgMenuGridO /></span>
+        </div>
       </div>
     </header>
   );
