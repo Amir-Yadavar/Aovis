89fb6222d3cccb536c0c56f4e3105239e055dd9f
amir <amiresya@gmail.com>
2024-07-06 20:06:08 +0330

    create content one component homepage ..

---
+++ components/Homepage/MovieNow/MovieNow.jsx
@@ -0,0 +1,9 @@
+import styles from '@/styles/movieNow.module.css'
+
+function MovieNow() {
+  return (
+    <section className={styles.containerMovieNow}></section>
+  )
+}
+
+export default MovieNow
