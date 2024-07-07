5abffd9b9be1668bbbdcb128c3772c539767d237
amir <amiresya@gmail.com>
2024-07-07 21:12:38 +0330

    create box of content1 homepage ..

--- components/Homepage/MovieNow/MovieNow.jsx
+++ components/Homepage/MovieNow/MovieNow.jsx
@@ -2,7 +2,14 @@ import styles from '@/styles/movieNow.module.css'
 
 function MovieNow() {
   return (
-    <section className={styles.containerMovieNow}></section>
+    <section className={styles.containerMovieNow}>
+      <div></div>
+      <div className='d-flex align-items-center justify-content-center flex-wrap my-4'>
+        <div className={styles.boxSec}></div>
+        <div className={styles.boxSec}></div>
+        <div className={styles.boxSec}></div>
+      </div>
+    </section>
   )
 }
 
