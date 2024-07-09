019f6afcb51e6503405676762bfc6712a8bc424e
amir <amiresya@gmail.com>
2024-07-09 19:57:44 +0330

    fix style ..

--- components/Homepage/MovieNow/MovieNow.jsx
+++ components/Homepage/MovieNow/MovieNow.jsx
@@ -1,16 +1,23 @@
-import styles from '@/styles/movieNow.module.css'
+import styles from "@/styles/movieNow.module.css";
 
 function MovieNow() {
   return (
     <section className={styles.containerMovieNow}>
       <div></div>
-      <div className='d-flex align-items-center justify-content-center flex-wrap my-4'>
-        <div className={styles.boxSec}></div>
-        <div className={styles.boxSec}></div>
-        <div className={styles.boxSec}></div>
+      <div className="d-flex align-items-center justify-content-center flex-wrap my-4">
+        <div className={styles.containerBoxSec}>
+          <div className={styles.boxSecImg}></div>
+        </div>
+        <div className={styles.containerBoxSec}>
+          <div className={styles.boxSecImg}></div>
+        </div>
+        <div className={styles.containerBoxSec}>
+          <div className={styles.boxSecImg}></div>
+        </div>
+       
       </div>
     </section>
-  )
+  );
 }
 
-export default MovieNow
+export default MovieNow;
