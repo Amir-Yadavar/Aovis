646e20ffeb2efd063dd5bd73bc78ac075e9759a2
amir <amiresya@gmail.com>
2024-07-18 13:21:15 +0330

    create dbFake ..

--- components/Homepage/CheckoutMovies/CheckoutMovies.jsx
+++ components/Homepage/CheckoutMovies/CheckoutMovies.jsx
@@ -1,6 +1,19 @@
+"use client";
+
 import BoxCheckoutMovies from "@/module/BoxCheckoutMovies/BoxCheckoutMovies";
 import styles from "@/styles/checkoutMovies.module.css";
 import { RiMovie2Fill } from "react-icons/ri";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
 function CheckoutMovies() {
   return (
     <section className={styles.container}>
@@ -21,26 +34,79 @@ function CheckoutMovies() {
             </p>
           </div>
         </div>
-
-        <div className="d-flex align-items-center justify-content-around flex-wrap">
-          <BoxCheckoutMovies
-            img="/img/header/movie-image1.jpg"
-            title="The Way Of Water"
-            category="Adventure , Crime"
-            min="120"
-          />
-          <BoxCheckoutMovies
-            img="/img/header/banner-02.jpg"
-            title="The Witcher Season 2"
-            category="Action"
-            min="60"
-          />
-          <BoxCheckoutMovies
-            img="/img/header/banner-01.jpg"
-            title="Wrong Turns Part 2"
-            category="Horror"
-            min="110"
-          />
+        <div className="d-none d-xxl-block">
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={30}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <BoxCheckoutMovies
+                img="/img/header/movie-image1.jpg"
+                title="The Way Of Water"
+                category="Adventure , Crime"
+                min="120"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <BoxCheckoutMovies
+                img="/img/header/banner-02.jpg"
+                title="The Witcher Season 2"
+                category="Action"
+                min="60"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <BoxCheckoutMovies
+                img="/img/header/banner-01.jpg"
+                title="Wrong Turns Part 2"
+                category="Horror"
+                min="110"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <BoxCheckoutMovies
+                img="/img/header/banner-02.jpg"
+                title="The Witcher Season 2"
+                category="Action"
+                min="60"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <BoxCheckoutMovies
+                img="/img/header/movie-image1.jpg"
+                title="The Way Of Water"
+                category="Adventure , Crime"
+                min="120"
+              />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+        <div className="d-xxl-none">
+          <div className="d-flex align-items-center justify-content-around flex-wrap">
+            <BoxCheckoutMovies
+              img="/img/header/movie-image1.jpg"
+              title="The Way Of Water"
+              category="Adventure , Crime"
+              min="120"
+            />
+            <BoxCheckoutMovies
+              img="/img/header/banner-02.jpg"
+              title="The Witcher Season 2"
+              category="Action"
+              min="60"
+            />
+            <BoxCheckoutMovies
+              img="/img/header/banner-01.jpg"
+              title="Wrong Turns Part 2"
+              category="Horror"
+              min="110"
+            />
+          </div>
         </div>
       </div>
     </section>
