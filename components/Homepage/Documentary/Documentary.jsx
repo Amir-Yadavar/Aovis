97506d45068ca8b01269ec8735f5eeb0d6fa92ce
amir <amiresya@gmail.com>
2024-07-27 20:04:38 +0330

    add content in homepage section 3 ..

--- components/Homepage/Documentary/Documentary.jsx
+++ components/Homepage/Documentary/Documentary.jsx
@@ -1,9 +1,42 @@
-import styles from "@/styles/documentary.module.css"
+import styles from "@/styles/documentary.module.css";
+import Image from "next/image";
+import { RiMovie2Fill } from "react-icons/ri";
 
 function Documentary() {
   return (
-    <section className={styles.container}></section>
-  )
+    <section className={styles.container}>
+      <div className="container  p-3">
+        <div className="my-4">
+          <span className="logo">
+            <RiMovie2Fill />
+          </span>
+          <h4 className="titleLogo">Documentary</h4>
+          <h3 className="textLogo text-light">Life Under an Umbrella</h3>
+
+          <p className="text-secondary">
+            Phasellus non cursus ligula, sed mattis urna. Aenean ac tor gravida
+            ,<br /> volutpat quam eget, consequat elit.
+          </p>
+          <div className="d-flex align-items-center my-2 containerAwards">
+          <Image
+            src={"/img/content3/awards-banner-01.png"}
+            alt="awards"
+            width={116}
+            height={95}
+            className="mx-3"
+          />
+          <Image
+            src={"/img/content3/awards-banner-02.png"}
+            alt="awards"
+            width={116}
+            height={95}
+            className="mx-3"
+          />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Documentary
+export default Documentary;
