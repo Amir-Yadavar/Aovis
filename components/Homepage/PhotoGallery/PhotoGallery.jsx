a5b0a4b52eba44bbbd4a95551359989f016a0544
amir <amiresya@gmail.com>
2024-08-18 22:02:23 +0330

    change style ..

--- components/Homepage/PhotoGallery/PhotoGallery.jsx
+++ components/Homepage/PhotoGallery/PhotoGallery.jsx
@@ -1,13 +1,13 @@
-import styles from "@/styles/photoGallery.module.css"
+import styles from "@/styles/photoGallery.module.css";
 
 function PhotoGallery() {
   return (
     <section className={styles.container}>
       <div className={styles.topSec}>
-        <h3>Our Photo Gallery</h3>
+        <div className={styles.divBackImg}></div>
       </div>
     </section>
-  )
+  );
 }
 
-export default PhotoGallery
+export default PhotoGallery;
